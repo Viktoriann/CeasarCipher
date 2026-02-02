87014f0c03c4a41ff6a662cdb1864e8c7a1dfbc8
Viktoria Nikolova <viktoriann99@gmail.com>
2026-02-02 19:12:26 +0200

    Main file updated

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def caesar(encode_or_decode, original_text, shift_amount):
     output_text = ""
 
-    for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
 
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
-caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
+
+should_continue = True
+
+while True:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+
+    if restart == "no":
+        print("Goodbye!")
+        should_continue = False
