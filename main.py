bc7c8f7f28ecfd1aefcb189cc8f58c089552cf16
Viktoria Nikolova <viktoriann99@gmail.com>
2026-02-02 15:08:13 +0200

    Created main python file

---
+++ main.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text):
+    result = ""
+
+    for char in text:
+
+        current_new_position = alphabet.index(char) + shift
+        current_new_char = alphabet[current_new_position]
+        result += current_new_char
+    print(result)
+
+encrypt(text)
+
