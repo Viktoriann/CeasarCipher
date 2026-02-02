75821b0c55fe22e5ab3a12702852ac392145681d
Viktoria Nikolova <viktoriann99@gmail.com>
2026-02-02 16:37:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text):
-    result = ""
+def caesar(encode_or_decode, original_text, shift_amount):
+    output_text = ""
 
-    for char in text:
+    for letter in original_text:
+        if encode_or_decode == "decode":
+            shift_amount *= -1
 
-        current_new_position = alphabet.index(char) + shift
-        current_new_char = alphabet[current_new_position]
-        result += current_new_char
-    print(result)
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
 
-encrypt(text)
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+caesar(encode_or_decode=direction, original_text=text, shift_amount=shift)
 
